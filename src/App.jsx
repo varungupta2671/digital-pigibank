73edb7cfbd470d35fae103e75fbbf35d753f0df4
Varun Gupta <varungupta2671@gmail.com>
2026-01-18 13:46:28 +0530

    Added speed insights

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { usePiggy, PiggyProvider } from './context/PiggyContext';
 import { ToastProvider } from './context/ToastContext';
 import { ThemeProvider } from './context/ThemeContext';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Header from './components/Header';
 import GoalForm from './components/GoalForm';
 import CreateGoal from './pages/CreateGoal';
@@ -52,6 +53,7 @@ function App() {
         <BrowserRouter>
             <ThemeProvider>
                 <ToastProvider>
+                    <SpeedInsights />
                     <PiggyProvider>
                         <AppContent />
                     </PiggyProvider>
